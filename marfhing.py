43c05ebbbfebe5d297a231b6928af776e3a12f59
dygkkh <158525086+dygkkh@users.noreply.github.com>
2024-06-22 17:52:36 -0300

    Update and rename interpreter.py to marfhing.py

--- marfhing.py
+++ marfhing.py
@@ -1,19 +1,21 @@
 import sys
+from Errors import *
+from stack import *
+try:
+ filepath = sys.argv[1]
 
-filepath = sys.argv[1]
-
-with open(filepath, "r") as file:
+ with open(filepath, "r") as file:
     program_lines = [
     line.strip()
     for line in file.readlines()]
 
-vars = {"au":"149.597.870,66 km",
+ vars = {"au":"149.597.870,66 km",
         "km":"1000m",
         "m":"100cm"}
-toks = []
-tok_count = 0
-label_tra = {}
-for line in program_lines:
+ toks = []
+ tok_count = 0
+ label_tra = {}
+ for line in program_lines:
     tok = line.split(" ")
     a = tok[0]
 
@@ -28,6 +30,7 @@ for line in program_lines:
     tok_count += 1
     if a.lower() == "put":
         num = int(tok[1])
+
         toks.append(num)
         tok_count += 1
     elif a.lower() == "res":
@@ -42,11 +45,11 @@ for line in program_lines:
         stri = " ".join(tok[1:])[1:-1]
         toks.append(stri)
         tok_count += 1
-    elif a.lower() == "goto-equ.0":
+    elif a.lower() == "if_addtok_equ.0":
         label = tok[1]
         toks.append(label)
         tok_count += 1
-    elif a.lower() == "goto-grth.0":
+    elif a.lower() == "if_addtok_grth.0":
         label = tok[1]
         toks.append(label)
         tok_count += 1
@@ -67,47 +70,14 @@ for line in program_lines:
             if tok[2] == "equ":
                 toks[name] = int(inv)
 
-class Errors:
-    def __init__(self, num, num2):
-        self.se = ("", "you forgot input", "incorrect Token", "incorrect function", "you forgot a token")
-        self.ffe = "you forgot or you type incorrectly the function 'endtok;'"
-        self.ve = "you write in the incorrect type"
-        self.errors = ("", "Syntax_Error", "Final-Function_Error", "Val_Error")
-        self.pos = num
-        self.pos2 = num2
-    def ErrorIndentificator(self):
-        error = 0
-        if self.pos == 1:
-            print(f"_{self.pos}_: {self.errors[self.pos]}: {self.se[self.pos2]}")
-            error += 1
-        elif self.pos == 2:
-            print(f"_{self.pos}_: {self.errors[self.pos]}: {self.ffe}")
-            error += 1
-        elif self.pos == 3:
-            print(f"_{self.pos}_: {self.errors[self.pos]}: {self.ve}")
-            error += 1
-        print(error)
-        sys.exit()
 
-
-class stack:
-    def __init__(self, size):
-        self.buf = [0 for _ in range(size)]
-        self.sp = -1
-    def put(self, num):
-        self.sp += 1
-        self.buf[self.sp] = num
-    def ret(self):
-        num = self.buf[self.sp]
-        self.sp += 1
-        return num
-    def up(self):
-        return self.buf[self.sp]
-
-pc = 0
-stack = stack(256)
-try:
- while toks[pc] != "endtok;":
+ val = []
+ pc = 0
+ stack = stack(256)
+ print("&: program_started\n")
+# python marfhing.py "C:\Users\Usuario\build\Desktop\Nueva carpeta (9)\Lean\codigo\programming languages\python\marfhinh\marfhingpl\marfhing\marfhingin\examples\probe.mrfng"
+ try:
+  while toks[pc] != "endtok;":
     a = toks[pc]
     pc += 1
 
@@ -115,6 +85,7 @@ try:
         num = toks[pc]
         pc += 1
         stack.put(num)
+        val.append(num)
     elif a.lower() == "ret":
         stack.ret()
 
@@ -123,12 +94,17 @@ try:
             na = stack.ret()
             nb = stack.ret()
             stack.put(nb+na)
+            val = sum(val)
         except ValueError:
             Errors(3,1).ErrorIndentificator()
     elif a.lower() == "sub":
         na = stack.ret()
         nb = stack.ret()
         stack.put(nb-na)
+    elif a.lower() == "val.show;":
+        print(val)
+    elif a.lower() == "val.empy;":
+        val = []
     elif a.lower() == "imprint":
        try:
         stri = toks[pc]
@@ -140,16 +116,17 @@ try:
             Errors(4, 1).ErrorIndentificator()
        except IndexError:
         Errors(1, 1).ErrorIndentificator()
-    elif a.lower() == "intinput":
+    elif a.lower() == "addtok_intinput":
         try:
             num = int(input())
             stack.put(num)
+            val.append(num)
         except ValueError:
             Errors(3,1).ErrorIndentificator()
         except IndexError:
             Errors(1,1).ErrorIndentificator()
 
-    elif a.lower() == "goto-equ.0":
+    elif a.lower() == "if_addtok_equ.0":
         num = stack.up()
         if num == 0:
             pc = label_tra[toks[pc]]
@@ -164,7 +141,7 @@ try:
             Errors(1,1).ErrorIndentificator()
         except ValueError:
             Errors(3,1).ErrorIndentificator()
-    elif a.lower() == "goto-grth.0":
+    elif a.lower() == "if_addtok_grth.0":
         num = stack.up()
         if num > 0:
             pc = label_tra[toks[pc]]
@@ -180,9 +157,10 @@ try:
         continue
     else:
         Errors(1,2).ErrorIndentificator()
-except IndexError:
+ except IndexError:
     Errors(2, 1).ErrorIndentificator()
 
-finally:
-    print("\nend_program")
-
+ finally:
+    print("\n&: end_program")
+except IndexError:
+    print("end")
